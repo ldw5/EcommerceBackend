19f83893c6fda3856af8b9ded3c3e71e7d327101
Landrell Williams <dontess5@yahoo.com>
2021-11-17 16:35:45 -0500

    finished routes for category

--- models/Category.js
+++ models/Category.js
@@ -10,10 +10,10 @@ Category.init(
       type: DataTypes.INTEGER,
       allowNull:false,
       primaryKey:true,
-      autoIncrement:true
+      autoIncrement:true,
     }, category_name:{
       type: DataTypes.STRING,
-      allowNull:false
+      allowNull:false,
     }
   },
   {
--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -2,27 +2,56 @@ const router = require('express').Router();
 const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
-
+// req is an object containing information about the HTTP request
+// res sends back the response requested
 router.get('/', (req, res) => {
   // find all categories
   // be sure to include its associated Products
+  Category.findAll({include:[Product],})
+  //.then takes a callback function and returns another Promise(!)
+  .then((categories)=>{res.json(categories)})
+  //.catch is an error object which picks up the errors to resolve
+  .catch((err)=>{res.status(500).json(err)})
 });
 
 router.get('/:id', (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  Category.findOne({include:[Product], 
+    where:{ id: req.params.id,}
+  //.then takes a callback function and returns another Promise(!)
+  }).then((category)=>{res.json(category)})
+  //.catch is an error object which picks up the errors to resolve
+    .catch((err)=>{res.status(500).json(err)})
 });
 
 router.post('/', (req, res) => {
   // create a new category
+  Category.create(req.body)
+  //.then takes a callback function and returns another Promise(!)
+  .then((category)=>res.status(200).json(category))
+  //.catch is an error object which picks up the errors to resolve
+  .catch((err)=>res.status(400).json(err))
 });
 
 router.put('/:id', (req, res) => {
   // update a category by its `id` value
+  Category.update(req.body,{
+    where:{ id: req.params.id,}
+  //.then takes a callback function and returns another Promise(!)
+  }).then((category)=>res.status(200).json(category))
+  //.catch is an error object which picks up the errors to resolve
+    .catch((err)=>res.status(400).json(err))
 });
 
 router.delete('/:id', (req, res) => {
   // delete a category by its `id` value
+  Category.destroy(req.body,{
+    where:{ id: req.params.id,}
+  //.then takes a callback function and returns another Promise(!)
+  }).then((category)=>res.status(200).json(category))
+  //.catch is an error object which picks up the errors to resolve
+    .catch((err)=>res.status(400).json(err))
 });
 
 module.exports = router;
